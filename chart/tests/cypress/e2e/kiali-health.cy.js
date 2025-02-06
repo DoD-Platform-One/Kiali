835b7808da04bc7ad08ac02fb3fea520648adc3d
Luis Gomez <8248-lgomez2@users.noreply.gitlab.example.com>
2025-02-06 20:53:23 +0000

    Resolve "Kiali migrate from using Cytoscape to Patternfly"

--- chart/tests/cypress/e2e/kiali-health.cy.js
+++ chart/tests/cypress/e2e/kiali-health.cy.js
@@ -4,8 +4,8 @@ Cypress.on('uncaught:exception', (err, runnable) => {
 
 before(() => {
   if (Cypress.env('keycloak_test_enable')) {
-        cy.visit(Cypress.env('url'))
-        cy.performKeycloakLogin(Cypress.env('tnr_username'), Cypress.env('tnr_password'))
+    cy.visit(Cypress.env('url'))
+    cy.performKeycloakLogin(Cypress.env('tnr_username'), Cypress.env('tnr_password'))
   }
 })
 
@@ -30,61 +30,103 @@ function collapseMenu() {
   })
 }
 
-// Basic test that validates pages are accessible, basic error check
-it('Check Kiali is accessible', function() {
-  cy.visit(Cypress.env('url'))
-  cy.title().should('contain', 'Kiali')
-  expandMenu();
-  cy.get('#traffic_graph_cy', { timeout: 60000 }).click();
-  cy.get('#applications', { timeout: 60000 }).click();
-  // Check for generic errors (this is the red circle that appears if any connectivity with Promtheus/Grafana/Istio is not working)
-  cy.get('svg[fill="var(--pf-global--danger-color--100)"]').should('not.exist');
-})
-
-// Allow these tests to be skipped with an env variable
-// These tests should only run in BB CI since nothing is istio injected in Package CI
-if (Cypress.env("check_data")) {
-  it('Check Kiali Graph page loads data', function() {
+describe('Kiali', function () {
+  // Basic test that validates pages are accessible, basic error check
+  it('loads when visited', function () {
     cy.visit(Cypress.env('url'))
-    cy.title().should("eq", "Kiali");
-    expandMenu();
-    cy.get('#traffic_graph_cy', { timeout: 60000 }).click();
-    collapseMenu();
-    cy.get('button[id="namespace-selector"').click()
-    cy.get('input[type="checkbox"][value="monitoring"]').check()
-    cy.get('button[id="refresh_button"').click({force: true})
-    // Check for graph side panel because the main graph is tricky to grab
-    cy.get('div[id="graph-side-panel"]', { timeout: 60000 }).should("be.visible")
   })
 
-  it('Check Kiali Applications page loads data', function() {
-    cy.visit(Cypress.env('url'))
-    cy.title().should("eq", "Kiali");
+  it('has a title containing "Kiali"', function () {
+    cy.title().should('contain', 'Kiali')
+  })
+
+  it('pops out the side menu', function () {
     expandMenu();
+  })
+
+  it('loads the traffic graph page', function () {
+    cy.get('#traffic_graph_pf', { timeout: 60000 }).click();
+  })
+
+  it('loads the applications page', function () {
     cy.get('#applications', { timeout: 60000 }).click();
-    collapseMenu();
-    cy.get('button[id="namespace-selector"]').click()
-    //Only check the box for monitoring if it isn't already selected from previous test
-    cy.get('input[type="checkbox"][value="monitoring"]').then(($checkbox) => {
-      if (!$checkbox.prop('checked')) {
-        cy.wrap($checkbox).check()
-      }
-    })
-    cy.get('button[id="refresh_button"]').click({force: true})
-    // This gets us to the prometheus application
-    cy.get('a').filter((index, element) => {
-      return element.textContent.trim() === 'prometheus';
-    }).click();
-    // Validate the graph is visible
-    cy.contains('.Empty Graph', { timeout: 60000 }).should('not.exist');
-    cy.contains('.Error loading Graph', { timeout: 60000 }).should('not.exist');
-    // Load the outbound metrics tab
-    // there's nothing easy to check on here since elements are dynamic but we can at least load the page for the video
-    cy.get('#pf-tab-2-basic-tabs').click()
-    // Load the tracing tab
-    cy.get('#pf-tab-3-basic-tabs').click()
-    // Validate that error is not displayed
-    // NOTE: we don't check for actual traces because there can be delays in them displaying on the webpage
-    cy.contains('Error fetching traces').should("not.exist")
   })
-}
+
+  it('displays no connectivity errors', function () {
+    // Check for generic errors (this is the red circle that appears if any connectivity with Promtheus/Grafana/Istio is not working)
+    cy.get('svg[fill="var(--pf-global--danger-color--100)"]').should('not.exist');
+  })
+
+  // Skip remaining tests if check_data is not set
+  // These tests should only run in BB CI since nothing is istio injected in Package CI
+  if (!Cypress.env("check_data")) {
+    return
+  }
+
+  context('check_data is set', function () {
+
+    it('pops out the side menu', function () {
+      expandMenu();
+    })
+
+    it('loads the traffic graph page', function () {
+      cy.get('#traffic_graph_pf', { timeout: 60000 }).click();
+    })
+
+    it('closes the side menu', function () {
+      collapseMenu();
+    })
+
+    it('filters to the monitoring namespace', function () {
+      cy.get('button[id="namespace-selector"').click()
+      cy.get('input[type="checkbox"][value="monitoring"]').check()
+      cy.get('button[id="refresh_button"').click({ force: true })
+    })
+
+    it('opens the graph side panel', function () {
+      cy.get('div[id="graph-side-panel"]').should("be.visible")
+    })
+
+    it('pops out the side menu', function () {
+      expandMenu();
+    })
+
+    it('loads the applications page', function () {
+      cy.get('#applications', { timeout: 60000 }).click();
+    })
+
+    it('filters to the monitoring namespace if not already filtered', function () {
+      collapseMenu();
+      cy.get('button[id="namespace-selector"]').click()
+      cy.get('input[type="checkbox"][value="monitoring"]').then(($checkbox) => {
+        if (!$checkbox.prop('checked')) {
+          cy.wrap($checkbox).check()
+        }
+      })
+      cy.get('button[id="refresh_button"]').click({ force: true })
+    })
+
+    it('loads the prometheus application page', function () {
+      cy.get('a').filter((index, element) => {
+        return element.textContent.trim() === 'prometheus';
+      }).click();
+    })
+
+    it('loads the prometheus graph', function () {
+      cy.contains('.Empty Graph', { timeout: 60000 }).should('not.exist');
+      cy.contains('.Error loading Graph', { timeout: 60000 }).should('not.exist');
+    })
+
+    it('loads the outbound metrics tab', function () {
+      cy.get('#pf-tab-2-basic-tabs').click()
+    })
+
+    it('loads the tracing tab', function () {
+      cy.get('#pf-tab-3-basic-tabs').click()
+    })
+
+    it('does not show any errors fetching traces', function () {
+      cy.contains('Error fetching traces').should("not.exist")
+    })
+  })
+})
