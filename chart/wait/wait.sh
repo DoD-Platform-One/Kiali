68913d1032e92029af11923813c95526d4b4ce03
Jimmy Bourque <jbourque@bridgephase.com>
2025-07-29 10:47:07 -0500

    Resolve "Increase Stability for Kiali Testing"

--- chart/wait/wait.sh
+++ chart/wait/wait.sh
@@ -5,17 +5,19 @@ wait_project() {
    if ! current_generation=$(kubectl -n kiali get deployment/kiali -o=jsonpath='{.status.observedGeneration}'); then
       current_generation=0
    fi
-   next_generation=$((current_generation + 1))
 
    # NOTE: Since this is a hard-coded string, it may need to be updated if the way Kiali reports its progress changes
    # If the upgrade CI job fails on future versions, this is likely the first place to check
    kubectl -n kiali wait --for=jsonpath='{.status.progress.message}'="6. Finished all resource creation" --all kiali --timeout=60s && echo "Kiali CR is ready"
 
-   if ! kubectl -n kiali wait --for=jsonpath='{.status.observedGeneration}'="$next_generation" deployment/kiali --timeout=180s 2>/dev/null; then
-      echo "No new Kiali deployment generation detected. Checking if existing pods are ready."
-   else
-      echo "New Kiali deployment generation detected. Waiting for new pods to be ready."
-   fi
+   elapsed_time=0
+   while [[ $(kubectl get deploy kiali -n kiali -o jsonpath='{.status.observedGeneration}') -le "$current_generation" && $elapsed_time -le 180 ]]; do
+      echo "Waiting for new deployment..."
+      sleep 5
+      elapsed_time=$((elapsed_time+5))
+   done
 
    kubectl -n kiali rollout status deployment/kiali --timeout=180s && echo "Kiali is ready"
 }
+
+wait_project
